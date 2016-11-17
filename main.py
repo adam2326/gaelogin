f2da71ef9f7da8dc053ff3baebf91b454f36e001
adam2326 <adampilz.2326@gmail.com>
2016-11-17 15:36:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def user_login():
-    return 'Welcome! <a href={}>Sign in or register</a>.'.format(users.create_login_url(request.path))
+    user = users.get_current_user()
+    if user:
+        nickname = user.nickname()
+        logout_url = users.create_logout_url('/')
+        greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+            nickname, logout_url)
+    else:
+        login_url = users.create_login_url('/')
+        greeting = '<a href="{}">Sign in</a>'.format(login_url)
+
+    return '<html><body>{}</body></html>'.format(greeting)
 
 
 @app.errorhandler(500)
