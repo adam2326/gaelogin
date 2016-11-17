96fd1092d6affb34fdf50ea8cd57f54e2335fb66
adam2326 <adampilz.2326@gmail.com>
2016-11-17 15:23:03 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import logging
+from flask import Flask,request
+from google.appengine.api import users
+import datetime
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def user_login():
+    return 'Welcome! <a href={}>Sign in or register</a>.'.format(users.create_login_url(request.path))
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
